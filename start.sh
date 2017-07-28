9dbe1c76ad9dc76337e4b1eb8889aba0f09a1cfd
Gergely Imreh <imrehg@gmail.com>
2017-07-28 13:53:18 +0100

    update start script

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+if [ -z "$YOUTUBE_STREAM_KEY" ]; then
 
-echo Live-stream secret: $1
+    echo "Please set the LIVE_STREAM_SECRET environment variable! (find it in stream dashboard / Encoder Setup section)"
+    while : ; do
+        echo "Idling..."
+        sleep 600
+    done
+else
+    echo Live-stream secret: ${YOUTUBE_STREAM_KEY}
+fi
 
-raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$1
-
+raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}"
