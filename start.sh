9eaad08dde32cfd8e4e7e58fc91c045f29df61ef
Gergely Imreh <imrehg@gmail.com>
2017-07-28 15:33:57 +0100

    fix up streaming parameter

--- start.sh
+++ start.sh
@@ -10,4 +10,4 @@ else
     echo Live-stream secret: ${YOUTUBE_STREAM_KEY}
 fi
 
-raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}"
+raspivid -o - -t 0 -w 1920 -h 1080 -fps 30 -b 8000000 -g 40 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}"
