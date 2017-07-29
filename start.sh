0f60633a514f5f9f6d9a4741a7dc3d6d01c208d2
Gergely Imreh <imrehg@gmail.com>
2017-07-29 14:05:12 +0100

    revert some changes, let's see

--- start.sh
+++ start.sh
@@ -11,5 +11,5 @@ else
 fi
 
 raspivid -o - -t 0 -w 1920 -h 1080 -fps 30 -b ${RASPIVID_BITRATE=4500000} -g 40 ${RASPIVID_EXTRA_FLAGS} | \
-  ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -thread_queue_size 512 -f h264_mmal -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 \
+  ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -thread_queue_size 512 -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 \
   "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}"
