8a586787edcbb1c2dc4c042c1c0343790c425ed4
Gergely Imreh <imrehg@gmail.com>
2017-07-29 18:49:26 +0100

    streaming settings adjustments

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ else
     echo Youtube live stream key: ${YOUTUBE_STREAM_KEY}
 fi
 
-raspivid -o - -t 0 -w 1920 -h 1080 -fps 25 -b ${RASPIVID_BITRATE=800000} -g 160 -if both ${RASPIVID_EXTRA_FLAGS} | \
-  ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -thread_queue_size 512 -f h264 -i pipe:0 -c:v copy  -c:a aac -ab 32k -g 160 -strict experimental -f flv -r 25 \
-  "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}"
+raspivid -o - -t 0 -w 1920 -h 1080 -fps 25 -b ${RASPIVID_BITRATE=800000} -g 50 -if both ${RASPIVID_EXTRA_FLAGS} | \
+  ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
+     -thread_queue_size 256 -f h264 -i pipe:0 -c:v copy -bufsize 1500k -c:a aac -ab 16k -strict experimental -f flv -r 25 -g 50 \
+     "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}"
